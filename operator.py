14911b70907fb3413d43fb04bf4312d3e5ea09c7
Kileab <82032086+Unity2033@users.noreply.github.com>
2022-12-11 21:29:44 +0900

    Operator Example

---
+++ operator.py
@@ -0,0 +1,19 @@
+x = 2
+y = 2
+
+print(x + y) # x와 y의 값을 더합니다.
+print(x - y) # x와 y의 값을 뺍니다.
+print(x * y) # x와 y의 값을 곱합니다.
+print(x / y)
+print(x % y)
+print(x ** y)
+
+
+name = "james"
+age = "nineteen"
+gender = "man"
+
+information = name + " " + age + " " + gender
+
+print(information)
+print(age * 3) # 문자열에 * 연산을 수행하게 되면 입력한 수만큼 곱해져서 문자열이 출력됩니다.
