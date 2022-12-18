523b398a9d97ef2996e8c239f907cafc3229ae24
Kileab <82032086+Unity2033@users.noreply.github.com>
2022-12-18 18:37:57 +0900

    Using Selenium and setting automatic login

--- operator.py
+++ operator.py
@@ -1,19 +1,40 @@
-x = 2
-y = 2
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import pyautogui
+import pyperclip
 
-print(x + y) # x와 y의 값을 더합니다.
-print(x - y) # x와 y의 값을 뺍니다.
-print(x * y) # x와 y의 값을 곱합니다.
-print(x / y)
-print(x % y)
-print(x ** y)
+# 셀레니옴 사용 방법
+# browser = webdriver.Chrome("c:chromedrive.exe")
+# browser.get("https://www.naver.com")
 
+url = "https://nid.naver.com/nidlogin.login?mode=form&url=https%3A%2F%2Fwww.naver.com"
 
-name = "james"
-age = "nineteen"
-gender = "man"
+loginBrowser = webdriver.Chrome("c:/chormediver.exe")
 
-information = name + " " + age + " " + gender
+# 웹 페이지가 로딩될 깨까지 최대 10초를 기다립니다.
+loginBrowser.implicitly_wait(10)
+# 웹 페이지 화면을 최대 크기로 설정합니다.
+loginBrowser.maximize_window()
+# 웹 페이지 열기
+loginBrowser.get(url)
 
-print(information)
-print(age * 3) # 문자열에 * 연산을 수행하게 되면 입력한 수만큼 곱해져서 문자열이 출력됩니다.
+# ID 입력과 ID Inputfeild 선택
+id = loginBrowser.find_element('id','id')
+
+pyperclip.copy("kileab")
+pyautogui.hotkey("ctrl", "v")
+time.sleep(1)
+
+# Password 설정
+password = loginBrowser.find_element('id','pw').click()
+pyperclip.copy("Pressure")
+pyautogui.hotkey("ctrl", "v")
+time.sleep(1)
+
+# 로그인 
+loginSelect = loginBrowser.find_element("id","log\.login").click()
+
+# 웹 브라우저 종료 방지
+while(True):
+    pass
