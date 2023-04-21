9726373a7ceca9be17ed9b1ce712a4b377a4698d
mol1986 <82032086+Unity2033@users.noreply.github.com>
2023-04-21 21:49:08 +0900

    (2023.4.21) 문자열

--- Source.c
+++ Source.c
@@ -1,130 +1,111 @@
 #include <stdio.h> 
+#include <limits.h>
+// #include 파일 처리 전처리문
+// 시스템 파일이나 사용자 정의 파일을 프로그램 소스에
+// 삽입하여 사용하기 위한 선언문입니다.
+
+// 전처리기란?
+// 프로그램이 컴파일되기 이전에 프로그램에 대한
+// 사전 처리하는 과정입니다.
+
+#pragma region 매크로
+   // 프로그램 내에서 특정한 데이터가 문자열로
+   // 정의되고 처리되는 과정입니다.
+
+   // 매크로는 컴파일러가 아닌 선행처리기에 의해서
+   // 처리되는 문장이기 때문에 명령문 끝에 ";"을
+   // 사용하지 않습니다.
+#define SIZE 20 
+
+   // 매크로는 메모리 공간을 가지고 있지 않습니다.
+
+#pragma endregion
+
 
 void main()	
 {
-#pragma region 배열
-	// 같은 자료형의 변수들로 이루어진
-	// 유한 집합입니다.
+#pragma region 문자열
+	// 연속적인 메모리 공간에 저장된 
+	// 문자 변수의 집합입니다.
 
-	//  0  1  2  3  4
-	// [ ][ ][ ][ ][ ]
-	// int array[5];
+	// 문자열의 경우 포인터를 이용하여 문자열 상수
+	// 가리키도록 설정할 수 있습니다.
+	// const char * name = "James";
 	// 
-	// // 배열의 경우 첫 번째 원소(index)는 0부터 시작합니다.
-	// array[0] = 100;
-	// array[1] = 200;
-	// array[2] = 300;
-	// array[3] = 400;
-	// array[4] = 500;
+	// // %s : 문자열을 출력하는 서식
+	// printf("name 변수의 값 : %p\n", name);
+	// printf("name 변수가 가리키는 값 : %s\n", name);
 	// 
-	// // 배열의 크기는 컴파일이 되는 시점부터 고정된 메모리 공간을
-	// // 가지게 됩니다.
-	// // array[5] = 600; (X)
+	// // 문자열 상수는 데이터 영역의 읽기 전용 공간에
+	// // 저장되기 때문에 문자열의 값을 변경할 수 없습니다.
+	// // *name = 'G';
 	// 
-	// for (int i = 0; i < 5; i++)
-	// {
-	// 	printf("array[%d]의 값 : %d\n", i, array[i]);
-	// }
+	// name = "Tom";
 	// 
-	// // [15.5f] [30.25f] [57.15f]
-	// 				    // [0]    [1]     [2]
-	// float itemList[3] = {15.5f, 30.25f, 57.15f};
+	// // 문자열은 맨 마지막에 무효의 문자(NULL)가 자동으로
+	// // 포합됩니다.
 	// 
-	// // 배열의 메모리 공간은 프로그램이 실행되는 동안 변경할 수 없습니다.
-	// for (int i = 0; i < 3; i++)
-	// {
-	// 	printf("array[%d]의 값 : %f\n", i, itemList[i]);
-	// }
-	// 
-	// // 배열의 크기는 생략할 수 있으며, 초기화 목록에서
-	// // 설정한 요소에 따라 배열의 크기가 결정됩니다.
-	// char string [ ] = { 'A','B','C' };
-	// 
-	// // 배열의 이름은 배열의 시작 주소를 가리킵니다.
-	// printf("string 배열의 주소 : %p\n", string);
-	// printf("string 배열 [0]의 주소 : %p\n", &string[0]);
-											   
-	// 배열과 포인터의 관계
-	// int data = 100;
-	// 
-	// int * ptr = &data;
-	// 
-	// printf("ptr 변수의 값 : %p\n", ptr);
-	// printf("ptr + 1 : %p\n", ptr + 1);
-	// 
-	// // 10 진수
-	// 
-	// // 2 진수
-	// 
-	// // 16 진수
-	// // 1 ~ 9 
-	// // 10 : A
-	// // 11 : B
-	// // 12 : C
-	// // 13 : D
-	// // 14 : E
-	// // 15 : F
-#pragma endregion
+	// // NULL 문자의 역할은 문자열의 끝을 알려주는 것입니다.
+	// printf("name 변수의 값 : %p\n", name);
+	// printf("name 변수가 가리키는 값 : %s\n", name);
 
-#pragma region 시프트 연산자
-	// 비트 값을 주어진 숫자만큼 부호 방향으로 
-	// 이동시키는 연산자입니다.
+    // 배열을 이용한 문자열
+	// char string [ ] = "Game";
+	// 
+	// // 문자열은 공백도 함께 메모리 공간에 포함됩니다.
+	// char string1 [ ] = { "App l\0e" };
+	// 
+	// // 문자 배열 사이에 무효의 문자를 넣게 되면
+	// // 무효의 문자까지만 문자열을 출력합니다.
+	// printf("string의 값 : %s\n", string);
+	// printf("string1의 값 : %s\n", string1);
+	// 
+	// string[1] = 'b';
+	// string[3] = 'o';
+	// 
+	// printf("string의 값 : %s\n", string);
 
-	// char value = 10; // 0000 1010
-	// 
-	// // >> : 비트값을 주어진 숫자만큼 오른쪽으로 이동시킵니다.
-	// printf("value 변수를 오른쪽으로 2번 비트 연산한 결과 : %d\n", value >> 2);
-	// printf("value 변수의 값 : %d\n", value);
-	// 
-	// // 0000 1010
-	// // 0000 0101 -> 오른쪽으로 1번 비트 연산할 결과
-	// // 0000 0010 
-	// 	  
-	// // << : 비트값을 주어진 숫자만큼 왼쪽으로 이동시킵니다.
-	// printf("value 변수를 왼쪽으로 3번 비트 연산한 결과 : %d\n", value << 3);
-	// 
-	// 0000 1010
-	// 0001 0100 -> 왼쪽으로 1번
-	// 0010 1000 -> 왼쪽으로 2번
-	// 0101 0000 -> 왼쪽으로 3번	64 + 16 
+	// char 배열은 포인터 상수입니다.
+	// string = "Content";
+	// string = string1;
 
 #pragma endregion
 
-#pragma region 홀수와 짝수
-	// 문제) 17 <- 입력
-	// 홀수 출력
+	// 매크로
+	// printf("매크로 SIZE의 값 : %d", SIZE);
 
-	// int count = 0;
-	// 
-	// scanf_s("%d", &count);
-	// 
-	// if (count % 2 == 0)
-	// {
-	// 	printf("짝수");
-	// }
-	// else
-	// {
-	// 	printf("홀수");
-	// }
+#pragma region 최댓값과 최솟값
+	// 배열[5] = { 10, 5, 6, 99, 1};
+
+#define ARRAYSIZE 5
+
+	int max = INT_MIN;
+	int min = INT_MAX;
+
+	int array[ARRAYSIZE] = { 10,5,6,99,1 };
+
+	for (int i = 0; i < ARRAYSIZE; i++)
+	{
+		// 최댓값
+		if (max < array[i])
+		{
+			max = array[i];
+		}
+
+		// 최솟값
+		if (min > array[i])
+		{
+			min = array[i];
+		}
+	}
+
+	printf("array의 최댓값 : %d, array의 최솟값 : %d", max, min);
+
+	// 최댓값 : 99
+
+	// 최솟값 : 1
 
 #pragma endregion
 
-#pragma region 네이밍 컨벤션
-
-     // camel case (카멜 표기법)
-	 // 첫 단어를 제외하고 첫 글자를 대문자로 
-     // 표기하는 표기법입니다.
-
-     // ex) int playerHealth;
-
-     // pascal case (파스칼 표기법)
-     // 단어의 첫 글자를 대문자로 표기하는 표기법입니다.
-
-     // ex) void SelectTarget() { }
-
-     // snake case (스네이크 표기법)
-     // 전부 소문자로 작성하고 단어 사이에 (_)를 표기하는 표기법입니다.
-	 // ex) api_url   
-#pragma endregion
 
 }
