28840952a1dd3c07038b2fb0a614697441be54fe
mol1986 <82032086+Unity2033@users.noreply.github.com>
2023-04-26 21:47:18 +0900

    (2023.4.26) 변수의 범위

--- Source.c
+++ Source.c
@@ -1,82 +1,130 @@
 #include <stdio.h> 
-#include "SoundManager.h"
 
-#pragma region 재귀 함수
-   // 어떤 함수에서 자신을 다시 호출하여
-   // 작업을 수행하는 함수입니다.
-void Recursion(int x)
-{
-	if (x == 1)
-	{
-		// 재귀 함수는 함수를 계속 호출하기 때문에
-		// 스택 영역에 메모리가 계속 쌓이게 되므로
-		// 스택 오버플로우가 발생합니다.
-		return;
-	}
 
-	printf("Recursion() 함수 호출\n");
-	
-	Recursion(x - 1);
-}
-#pragma endregion
+#pragma region 전역 변수
+   // 함수 외부에서 선언된 변수로 프로그램 어디에서나 접근 가능하며,
+   // 프로그램이 종료되어야만 메모리에서 해제되는 변수입니다.
 
-#pragma region 팩토리얼
-
-int Factorial(int x)
-{
-	// 특정한 조건이 되었을 때 return 값
-	if (x == 1)
-	{
-		return 1;
-	}
-	// 특정한 조건이 아니라면 재귀 함수 실행
-	else	 
-	{
-		return x * Factorial(x - 1);
-	}
-}
-#pragma endregion
-
-#pragma region 인라인 함수
-  // 함수를 호출하는 대신 함수가 호출되는 위치마다
-  // 함수의 코드를 복사하여 전달하는 방식의 함수입니다.
-inline void Function()
-{
-	// 인라인 함수는 함수를 호출하는 과정이 없으므로
-	// 처리 속도가 빠르지만, 인라인 함수를 많이 사용하게 
-	// 되면 함수의 코드가 복사되기 때문에 실행 파일의 
-	// 커지게 됩니다.
-	printf("Function() 호출\n");	
-}
+int globalValue = 5;
 
 #pragma endregion
+
+#pragma region 정적 변수
+    // 지역 변수와 전역 변수의 특징을 가지고 있으며, 
+    // 한 번만 초기화가 이루어집니다.
+
+static int attack;
+
+void Calculator()
+{
+	static int value = 1;
+
+	value += 1;
+
+	printf("정적 변수 value의 값 : %d\n", value);
+}
+
+
+#pragma endregion
+
+void Function()
+{	
+	int count = 0;
+
+	count += 1;
+	globalValue += 1;
+
+	printf("count의 값 : %d\n", count);
+	printf("globalValue의 값 : %d\n", globalValue);
+}
 
 void main()	
 {    
-	// 재귀 함수
-	// Recursion(3);
+#pragma region 지역 변수
+	// '블록'{ } 내에서 선언된 변수로
+	// 블록 내에서만 유효하며, 블록이 종료되면
+	// 메모리에서 사라지는 변수입니다.
 
-	// 팩토리얼
-    // printf("Factorial의 값 : %d", Factorial(6));
-
-	// 인라인 함수
-	// Function();
-
-#pragma region 반복문 (do~while)
-// 조건과 상관없이 한 번의 작업을 수행한 다음
-// 조건에 따라 명령문을 실행하는 반복문입니다.
-
-	// int count = 3;
+	// A 지역
+	// int data = 100;
 	// 
-	// do
-	// {
-	// 	printf("로그인 시도\n");
-	// 	count--;
-	// } 
-	// while (count > 0);
+	// { // B 지역
+	// 	int data = 20;
+	// 	printf("B 지역 data의 값 : %d\n", data);
+	// }
+	// 
+	// printf("A 지역 data의 값 : %d\n", data);
 #pragma endregion
 
-	Sound();
+	// Function();
+	// Function();
+	// 
+	// Calculator();
+	// Calculator();
+	// 
+	// printf("정적 변수 attack의 값 : %d", attack);
+	
+#pragma region 범용 포인터
+	// 자료형이 정해지지 않은 상태로 모든 자료형을 
+	// 저장할 수 있는 포인터입니다.
+
+	// char charData = 'X';
+	// int intData = 10;
+	// 
+	// void * ptr = NULL;
+	// 
+	// // 범용 포인터는 메모리 주소에 접근해서 값을
+	// // 변경할 수 없습니다.
+	// ptr = &charData;
+	// 
+	// // 범용 포인터로 변수의 메모리에 접근하려면 범용
+	// // 포인터가 가리키는 변수의 자료형으로 형 변환
+	// // 해주어야 합니다.
+	// *(char *)ptr = 'M';
+	// printf("ptr이 가리키는 값 : %c\n", *(char*)ptr);
+	// 
+	// ptr = &intData;
+	// 
+	// *(int*)ptr = 99;
+	// printf("ptr이 가리키는 값 : %d\n", *(int*)ptr);
+#pragma endregion
+
+#pragma region 약수
+	// 문제) 내가 입력한 숫자의 약수를 출력하세요.
+	// 12 -> 1, 2, 3, 4, 6, 12
+
+	// int number = 0;
+	// 
+	// scanf_s("%d", &number);
+	// 
+	// for (int i = 1; i <= number; i++)
+	// {
+	// 	if (number % i == 0)
+	// 	{					
+	// 		// 1, 2, 3, 4, 6, 12
+	// 		printf("%d ", i);
+	// 	}
+	// }
+#pragma endregion
+
+#pragma region ShortCircuit
+	// 논리 연산에서 두 피연산자 중 어느 한쪽만 '참'
+	// 이면 우측의 피연산자의 값은 평가하지 않고 바로 결과를
+	// 얻는 행위입니다.
+
+	// &&, || (논리 연산자)
+	// &, |	  (비트 연산자)
+
+	int x = 0;
+	int y = 1;
+
+	if (x == -1 && ++y == 2) { }
+
+	printf("x의 값 : %d, y의 값 : %d\n", x,y);
+
+
+
+#pragma endregion
 
 
 }
