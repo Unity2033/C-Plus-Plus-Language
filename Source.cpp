8aff196eec69167f4502f3210fbbaacdfafb9e43
mol1986 <82032086+Unity2033@users.noreply.github.com>
2023-05-22 21:48:20 +0900

    (2023.5.22) 미로 찾기 게임 1

--- Source.cpp
+++ Source.cpp
@@ -4,195 +4,134 @@
 #include <conio.h>
 #include <windows.h>
 
-#pragma comment (lib, "winmm.lib")
-#include <mmsystem.h>
-
 #define UP 72
 #define LEFT 75
 #define RIGHT 77
 #define DOWN 80
 
-// HANDLE 인덱스에 접근해서 버퍼를 교체시키는 변수
-int screenIndex = 0;
-
-// 버퍼의 크기
-int width = 100;
-int height = 60;
-
-// 버퍼 생성
-HANDLE Screen[2]; // [0] front buffer
-                  // [1] back buffer
+#define WIDTH 11
+#define HEIGHT 11
 
 typedef struct Player
 {
-	int x, y;
+	int x;
+	int y;
 	const char * shape;
 }Player;
 
-// 버퍼를 초기화하는 함수
-void ScreenInit()
+// 좌표 이동 함수
+void GotoXY(int x, int y)
 {
-	CONSOLE_CURSOR_INFO cursor;
+	COORD position = { x, y };
 
-	// 버퍼의 가로 사이즈, 세로 사이즈
-	COORD size = { width, height };
-
-	// Left, Top, Right, Bottom
-	SMALL_RECT rect = { 0, 0, width - 1, height - 1 };
-
-	// 화면 2개를 생성합니다.
-	// front buffer
-	Screen[0] = CreateConsoleScreenBuffer
-	(
-		GENERIC_READ | GENERIC_WRITE,
-		FILE_SHARE_READ | FILE_SHARE_WRITE,
-		NULL,
-		CONSOLE_TEXTMODE_BUFFER,
-		NULL
-	);
-
-	SetConsoleScreenBufferSize(Screen[0], size);
-
-	SetConsoleWindowInfo(Screen[0], TRUE, &rect);
-
-	// back buffer
-	Screen[1] = CreateConsoleScreenBuffer
-	(
-		GENERIC_READ | GENERIC_WRITE,
-		FILE_SHARE_READ | FILE_SHARE_WRITE,
-		NULL,
-		CONSOLE_TEXTMODE_BUFFER,
-		NULL
-	);
-
-	SetConsoleScreenBufferSize(Screen[1], size);
-
-	SetConsoleWindowInfo(Screen[1], TRUE, &rect);
-
-	// 커서의 활성화 여부
-	// false : 거짓
-	// true : 참
-	cursor.bVisible = false;
-
-	SetConsoleCursorInfo(Screen[0], &cursor);
-	SetConsoleCursorInfo(Screen[1], &cursor);
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), position);
 }
 
-// 버퍼를 교체하는 함수
-void ScreenFlipping()
-{
-	// 버퍼는 하나만 활성화시킬 수 있습니다.
-	SetConsoleActiveScreenBuffer(Screen[screenIndex]);
+// 미로 맵 데이터
+char maze[WIDTH][HEIGHT];
 
-	screenIndex = !screenIndex;
+// 미로 맵 생성
+void CreateMaze()
+{
+	// 0 : 빈 공간 ("  ")
+	// 1 : 벽 (□)
+	// 2 : 탈출구 (◎)
+
+	strcpy(maze[0],  "1111111111");
+	strcpy(maze[1],  "1000011111");
+	strcpy(maze[2],  "1101011111");
+	strcpy(maze[3],  "1111011111");
+	strcpy(maze[4],  "1100001111");
+	strcpy(maze[5],  "1101101111");
+	strcpy(maze[6],  "1101100011");
+	strcpy(maze[7],  "1101111011");
+	strcpy(maze[8],  "1100011011");
+	strcpy(maze[9],  "1111011002");
+	strcpy(maze[10], "1111111111");
 }
 
-// 교체된 버퍼를 지워주는 함수
-void ScreenClear()
+void Renderer()
 {
-	COORD coord = { 0,0 };
+	for (int i = 0; i < WIDTH; i++)
+	{
+		for (int j = 0; j < HEIGHT; j++)
+		{
+			if (maze[i][j] == '0')
+			{
+				printf("  ");
+			}
+			else if (maze[i][j] == '1')
+			{
+				printf("■");
+			}
+			else if (maze[i][j] == '2')
+			{
+				printf("◎");
+			}
+		}
 
-	DWORD dw;
-
-	FillConsoleOutputCharacter
-	(
-	    Screen[screenIndex],
-	    ' ',
-	    width * height,
-		coord,
-		&dw
-	);
+		printf("\n");
+	}
 }
 
-// 버퍼를 해제하는 함수
-void ScreenRelease()
+void Keyboard(char map[WIDTH][HEIGHT], Player * ptrPlayer)
 {
-	CloseHandle(Screen[0]);
-	CloseHandle(Screen[1]);
-}
+	char key = 0;
 
-// 더블 버퍼링을 이용해서 출력하는 함수
-void ScreenPrint(int x, int y, const char * string)
-{
-	COORD cursorPosition = { x, y };
+	if (_kbhit())
+	{
+		key = _getch();
 
-	DWORD dw;
+		if (key == -32)
+		{
+			key = _getch();
+		}
 
-	SetConsoleCursorPosition(Screen[screenIndex], cursorPosition);
+		switch (key)
+		{
+			case UP : if (maze[ptrPlayer->y - 1][ptrPlayer->x / 2] != '1')
+			{
+				ptrPlayer->y--;
+			}
+			break;
+			case LEFT :	if (maze[ptrPlayer->y][ptrPlayer->x / 2 - 1] != '1')
+			{
+				ptrPlayer->x -= 2;
+			}
+			break;
+			case RIGHT : if (maze[ptrPlayer->y][ptrPlayer->x / 2 + 1] != '1')
+			{
+				ptrPlayer->x += 2;
+			}
+			break;
+			case DOWN :	if (maze[ptrPlayer->y + 1][ptrPlayer->x / 2] != '1')
+			{
+				ptrPlayer->y++;
+			}
+			break;
+		}
+	}
 
-	WriteFile
-	(
-		Screen[screenIndex],
-		string,
-		strlen(string),
-		&dw,
-		NULL
-	);
 }
 
 int main()
 {
-	// PlaySound(TEXT("Sound.wav"), NULL, SND_ASYNC); // 일반 재생
-	PlaySound(TEXT("Sound.wav"), NULL, SND_ASYNC | SND_LOOP); // 반복 재생
+	Player player = { 2, 6, "★"};
 
-	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 1);
-	Player player = { 5, 5, "★"};
-
-	// 1. 버퍼 초기화
-	ScreenInit();
-
-	char key = 0;
-							
-	// 색상을 설정하는 함수
-	// 0 = 검정  
-	// 1 = 블루	 	    
-	// 2 = 초록
-	// 3 = 옥색
-	// 4 = 빨강
-	// 5 = 자주
-	// 6 = 노랑
-	// 7 = 흰색
-	// 8 = 회색
-
+	CreateMaze();
 
 	while (1)
 	{
-		if (_kbhit())
-		{
-				key = _getch();	// 방향키 입력 시 224 00이 들어오게 되므로 앞에 있는 값 224를 삭제합니다.
-						
-				if (key == -32)
-				{
-				 	key = _getch();
-				}
-				 
-				switch (key)
-				{
-					case UP : if(player.y > 0) player.y--;
-						break;
-					case LEFT : if(player.x > 0) player.x--;
-					 	break;
-					case RIGHT: player.x++;
-					 	break;
-					case DOWN: player.y++;
-					 	break;
-				}
-		
-				system("cls");
-		}
+		Renderer();
 
-		ScreenPrint(player.x, player.y, player.shape);
-	    
-		// 2. 버퍼 교체 
-		ScreenFlipping();
+		Keyboard(maze, &player);
 
-		// 3. 교체된 버퍼의 내용을 삭제
-		ScreenClear();
+		GotoXY(player.x, player.y);
+		printf("%s", player.shape);
+
+		Sleep(100);
+		system("cls");
 	}
-
-	// 게임이 종료되었을 때 버퍼를 해제합니다.
-	ScreenRelease();
-
 
 	return 0;
 }
