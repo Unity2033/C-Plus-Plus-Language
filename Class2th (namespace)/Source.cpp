be55cc841ace43016a5c907f5dcdf0c70022ee7b
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2024-02-13 21:42:45 +0900

    (2024.2.13) 클래스

---
+++ Class2th (namespace)/Source.cpp
@@ -0,0 +1,105 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 
+#include <iostream>
+#include <string.h>
+
+char character = 'A';
+using namespace std;
+
+namespace EnemySKill
+{
+	void Skill()
+	{
+		cout << "Enemy Skill" << endl;
+	}
+}
+
+using namespace EnemySKill;
+
+namespace PlayerSkill
+{
+	void Skill()
+	{
+		cout << "Player Skill" << endl;
+	}
+}
+
+
+int main()
+{
+#pragma region 범위 지정 연산자
+	// 여러 범위에서 사용되는 식별자를 구분하는데
+	// 사용하는 연산자입니다.
+
+	// char character = 'W';
+	// 
+	// std::cout << "character의 값 : " << character << std::endl;
+	// 
+	// // 범위 지정 연산자는 전역 변수와 같은 이름의 지역 변수가 선언되었을 때
+	// // 가장 가까운 범위에 선언된 변수의 이름을 사용하는 범위 규칙이 존재하기
+	// // 때문에 전역 변수가 호출되지 않습니다.
+	// 
+	// std::cout << "전역 변수 character의 값 : " << ::character << std::endl;
+	// 
+	// 범위 지정 연산자를 사용하여 전역 변수의 범위를 지정하게 되면 전역 범위에
+	// 접근할 수 있습니다.
+
+#pragma endregion
+
+#pragma region 이름 공간
+	// 속성을 구분할 수 있도록 유효 범위를 설정하는 영역입니다.
+
+	// Skill();
+	// PlayerSkill::Skill();
+
+	// 이름 공간은 같은 이름의 함수나 변수를 또 다른 공간에
+	// 선언하여 중복되지 않도록 선언할 수 있습니다.
+
+#pragma endregion
+
+#pragma region 회문 판별
+
+	/*bool flag = true;
+
+	const char * content = "levwdwtwtel";
+
+	int count = strlen(content);
+
+	for (int i = 0; i < count / 2; i++)
+	{
+		if (content[i] != content[count - 1 - i])
+		{
+			flag = false;
+			break;
+		}
+	}
+
+	if (flag == true)
+	{
+		cout << "회문입니다." << endl;
+	}
+	else
+	{
+		cout << "회문이 아닙니다." << endl;
+	}*/
+
+#pragma endregion
+
+#pragma region 소문자를 대문자로 변환하는 알고리즘
+
+	//char nickName [ ] = "bard";
+
+	//for (int i = 0; i < strlen(nickName); i++)
+	//{
+	//	   if (nickName[i] >= 'a' && nickName[i] <= 'z')
+	//	   {
+	//	   	   nickName[i] -= 32;
+	//	   }
+	//}
+	//	  
+	//cout << "nickName의 값 : " << nickName << endl;
+
+#pragma endregion
+
+
+	return 0;
+}
---
+++ Class3th (class)/Source.cpp
@@ -0,0 +1,96 @@
+﻿#include <iostream>
+using namespace std;
+
+#pragma region 클래스
+// 사용자 정의 데이터 유형으로 속성과 함수가 포함
+// 되어 있으며, 클래스를 통해 객체를 생성하여 접근
+// 하여 사용할 수 있는 집합체입니다.
+
+class GameObject
+{
+#pragma region 접근 지정자
+	// 클래스 내부의 포함되어 있는 속성에 접근 범위를
+	// 제한하는 지정자입니다.
+
+	// public    : 클래스 내부에 있는 데이터를 클래스 외부와 자기가 
+	//             상속하고 있는 클래스에서도 접근을 허용하는 지정자입니다.
+
+	// protected : 클래스 내부에 있는 데이터를 클래스 내부와 자기가 상속하고
+	//             있는 클래스에서만 접근을 허용하는 지정자입니다.
+
+	// private   : 클래스 내부에 있는 데이터를 클래스 내부에서만 접근을
+	//             허용하는 지정자입니다.  
+
+#pragma endregion
+	int x = 5;
+private:
+	int y = 10;
+protected:
+	int z = 15;
+public:
+	void Information()
+	{
+		cout << "x의 값 : " << x << endl;
+		cout << "y의 값 : " << y << endl;
+		cout << "z의 값 : " << z << endl;
+	}
+};
+#pragma endregion
+
+class Item
+{
+public:
+	// 생성자의 경우 객체가 생성될 때 단 한 번만 호출되며,
+	// 생성자는 반환형이 존재하지 않기 때문에 생성자가 호출되기
+	// 전에는 객체에 대한 메모리는 할당하지 않습니다.
+
+	Item()
+	{
+		cout << "Create Item" << endl;
+	}
+
+#pragma region 소멸자
+	// 객체가 소멸될 때 자동으로 실행되는 클래스의 멤버 함수입니다.
+
+	~Item()
+	{
+		cout << "Release Item" << endl;
+	}
+
+	// 소멸자는 객체가 메모리에서 해제될 때 단 한 번만
+	// 호출되며, 소멸자에는 매개 변수를 생성하여 사용할
+	// 수 없습니다.
+
+#pragma endregion
+
+};
+
+int main()
+{
+#pragma region 클래스
+	// GameObject gameObject;
+	// 
+	// cout << "GameObject의 크기 : " << sizeof(gameObject) << endl;
+	// 
+	// gameObject.Information();
+	// 
+	// GameObject * pointer = new GameObject;
+	// 
+	// pointer->Information();
+#pragma endregion
+
+#pragma region 생성자
+	// 클래스의 인스턴스가 생성되는 시점에서 자동으로
+	// 호출되는 특수한 멤버 함수입니다.
+
+	Item item;
+
+	Item* itemPtr = new Item;
+
+	delete itemPtr;
+
+#pragma endregion
+
+
+	return 0;
+}
