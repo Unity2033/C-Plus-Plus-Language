f31395b545506028b0b8eac29992f3f10f439ea0
Unity2033 <kileab@naver.com>
2024-02-28 21:04:33 +0900

    (2024.2.28) 가상 상속

---
+++ Class14th (Virtual Inheritance)/Class14th (Virtual Inheritance).cpp
@@ -0,0 +1,100 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Food
+{
+public:
+	Food()
+	{
+		cout << "Create Food" << endl;
+	}
+
+	~Food()
+	{
+		cout << "Release Food" << endl;
+	}
+};
+
+class Meat : public virtual Food
+{
+public:
+	Meat()
+	{
+		cout << "Create Meat" << endl;
+	}
+
+	~Meat()
+	{
+		cout << "Release Meat" << endl;
+	}
+};
+
+class Vegetable : public virtual Food
+{
+public:
+	Vegetable()
+	{
+		cout << "Create Vegetable" << endl;
+	}
+
+	~Vegetable()
+	{
+		cout << "Release Vegetable" << endl;
+	}
+};
+
+class Soup : public Meat, public Vegetable
+{
+public:
+	Soup()
+	{
+		cout << "Create Soup" << endl;
+	}
+
+	~Soup()
+	{
+		cout << "Release Soup" << endl;
+	}
+
+};
+
+int main()
+{
+#pragma region 가상 상속
+
+	// Soup soup;
+#pragma endregion
+
+#pragma region 등차수열
+
+	//	int firstTerm = 0;
+	//	int commonDifferent = 0;
+	//	
+	//	int n = 0;
+	//	
+	//	cout << "첫 항의 값 입력 : ";
+	//	cin >> firstTerm;
+	//	
+	//	cout << endl;
+	//	
+	//	cout << "공차의 값 입력 : ";
+	//	cin >> commonDifferent;
+	//	
+	//	cout << endl;
+	//	
+	//	cout << "n의 값 입력 : ";
+	//	cin >> n;
+	//	
+	//	cout << endl;
+	//	
+	//	for (int i = 0; i < n; i++)
+	//	{
+	//		cout << firstTerm + commonDifferent * i << " ";
+	//	}
+	//		
+
+#pragma endregion
+
+	return 0;
+}
