34b21d9111809e8e2c441180a649b2c935bcc9b2
Unity2033 <kileab@naver.com>
2024-06-13 21:45:56 +0900

    (2024.6.13) 싱글톤 패턴

---
+++ Game/Assets/Scripts/Managers/SceneMove.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneMove : MonoBehaviour
+{   
+    public void NextScene()
+    {
+        SceneManager.LoadScene("Game");
+    }
+}
